b235d2a3eccf50c13d81c26501c0b1a45b263605
Cisco Admin <prne@cisco.com>
2025-06-17 10:38:20 +0000

    Initial commit

---
+++ te_wrapper/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "0.1.0"
+
+from .auth import ThousandEyesAuth
+from .api import ThousandEyesAPI
+from .models import TestResult
+
+class ThousandEyes:
+    """
+    A wrapper class for the ThousandEyes API.
+    """
+    def __init__(self, api_token: str = None):
+        """
+        Initializes the ThousandEyes wrapper.
+
+        Args:
+            api_token: The ThousandEyes API token. If not provided, it attempts
+                       to read from the TE_API_TOKEN environment variable.
+        """
+        self.auth = ThousandEyesAuth(api_token=api_token)
+        self.api = ThousandEyesAPI(self.auth)
+
+__all__ = [
+    "ThousandEyes",
+    "__version__",
+]
